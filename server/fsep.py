c129dc0f924f39b649a39aba264cb4e8b664f170
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-02-20 22:00:27 -0300

    ✨feat: add concurrency control to message processing

--- server/fsep.py
+++ server/fsep.py
@@ -4,10 +4,12 @@
 
 import os
 import json
+import threading
 from client.client import DOWNLOAD_DIR
 from server.data_log import save_log
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data/files")
+arquivo_lock = threading.Lock()
 
 def processar_mensagem(mensagem, client_socket):
     try:
@@ -21,23 +23,28 @@ def processar_mensagem(mensagem, client_socket):
             nome_arquivo = dados["arquivo"]
             caminho_arquivo = os.path.join(DATA_DIR, os.path.basename(nome_arquivo))
             conteudo = dados["conteudo"]
-            with open(caminho_arquivo, "w") as f:
-                f.write(conteudo)
-            resposta = {"status": "ok", "mensagem": f"Arquivo '{nome_arquivo}' salvo no servidor."}
-            save_log(nome_arquivo,client_socket)
+            try:
+                with arquivo_lock:
+                    with open(caminho_arquivo, "w") as f:
+                        f.write(conteudo)
+                    resposta = {"status": "ok", "mensagem": f"Arquivo '{nome_arquivo}' salvo no servidor."}
+                    save_log(nome_arquivo,client_socket)
+            except FileExistsError:
+                resposta = {"status": "erro", "mensagem": "Arquivo já existe."}
 
         elif dados["comando"] == "BAIXAR":
             nome_arquivo = dados["arquivo"]
             caminho_arquivo = os.path.join(DATA_DIR, os.path.basename(nome_arquivo))
             caminho_cliente = os.path.join(DOWNLOAD_DIR, os.path.basename(nome_arquivo))
             try:
-                with open(caminho_arquivo, "rb") as src, open(caminho_cliente, "wb") as dst:
-                    while True:
-                        bloco = src.read(4096)
-                        if not bloco:
-                            break
-                        dst.write(bloco)
-                resposta = {"status": "ok", "mensagem": f"Arquivo '{nome_arquivo}' baixado com sucesso."}
+                with arquivo_lock:
+                    with open(caminho_arquivo, "rb") as src, open(caminho_cliente, "wb") as dst:
+                        while True:
+                            bloco = src.read(4096)
+                            if not bloco:
+                                break
+                            dst.write(bloco)
+                    resposta = {"status": "ok", "mensagem": f"Arquivo '{nome_arquivo}' baixado com sucesso."}
             except FileNotFoundError:
                 resposta = {"status": "erro", "mensagem": "Arquivo não encontrado."}
 
@@ -47,7 +54,8 @@ def processar_mensagem(mensagem, client_socket):
     except json.JSONDecodeError:
         resposta = {"status": "erro", "mensagem": "Formato de mensagem inválido."}
 
-    client_socket.sendall(json.dumps(resposta).encode()) #Comentar em caso de TESTES RÁPIDOS onde o cliente não é necessário.
+    # client_socket.sendall(json.dumps(resposta).encode()) #Comentar em caso de TESTES RÁPIDOS onde o cliente não é necessário.
+    print(resposta)
 
 # Testes rápidos
 if __name__ == "__main__":
