0373da5eb0ce959dfc7e4c92b615032eee2d54ee
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-03-20 01:45:14 -0300

    ♻️refactor: permitir configuração dinâmica do servidor via argumentos de linha de comando

--- client/client.py
+++ client/client.py
@@ -1,16 +1,14 @@
 import socket
 import json
 import os
-import datetime
-import csv
+import sys
 from tkinter import filedialog
 from status.protocolError import error_dict
 from status.protocolOk import ok_dict
 
 
-
-HOST_SRV = "192.168.0.4"
-PORT_SRV = 8080
+HOST_SRV = sys.argv[1]
+PORT_SRV = sys.argv[2]
 BUFFER_SIZE = 4096
 DOWNLOAD_DIR = os.path.join(os.path.dirname(__file__), "downloads")
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
